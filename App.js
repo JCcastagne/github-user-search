3e1aeec6bcdb8ef3d04439632e2e86f0a57a6352
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2023-01-26 11:25:47 -0500

    Create custom header; replaces default header from expo.

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, CommonActions } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { View, Text } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import { ThemeProvider } from './ThemeContext'
 import Home from './screens/Home'
 import User from './screens/User'
+import Users from './screens/Users'
 
 const Stack = createNativeStackNavigator()
+
+function CustomHeader ({ navigation, route }) {
+  console.log(route)
+  return (
+    <View style={{ paddingTop: 44, minHeight: 48, backgroundColor: '#1999' }}>
+      {route.name === 'Home' ? (
+        <Text>Github user search</Text>
+      ) : (
+        <MaterialCommunityIcons
+          name='arrow-left'
+          onPress={() => navigation.dispatch(CommonActions.goBack())}
+          size={24}
+          color='black'
+          style={{ padding: 17 }}
+        />
+      )}
+    </View>
+  )
+}
 
 export default function App () {
   return (
@@ -15,12 +37,26 @@ export default function App () {
           <Stack.Screen
             name='Home'
             component={Home}
-            options={{ headerShown: false }}
+            options={{
+              headerShadowVisible: false,
+              header: props => <CustomHeader {...props} />
+            }}
           />
           <Stack.Screen
             name='User'
             component={User}
-            options={{ headerShown: false }}
+            options={{
+              headerShadowVisible: false,
+              header: props => <CustomHeader {...props} />
+            }}
+          />
+          <Stack.Screen
+            name='Users'
+            component={Users}
+            options={{
+              headerShadowVisible: false,
+              header: props => <CustomHeader {...props} />
+            }}
           />
         </Stack.Navigator>
       </NavigationContainer>
