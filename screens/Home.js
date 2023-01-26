241abae3d67cf7ac5a4798a27236ce5b2fb7ae24
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2023-01-25 19:59:34 -0500

    Add logic to fetch searched github users.

--- screens/Home.js
+++ screens/Home.js
@@ -1,23 +1,69 @@
 import React from 'react'
 import { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView, StyleSheet, Text, View, TextInput } from 'react-native'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button
+} from 'react-native'
 
 export default function Home () {
-  const [searchInput, setSearchInput] = useState('Search for a user')
+  const [searchInput, setSearchInput] = useState('')
+  const [searchResults, setSearchResults] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  function handleErrors (error) {
+    console.log('ERROR: ', error)
+  }
+
+  async function handleSearch () {
+    setIsLoading(true)
+    console.log('searchInput: ', searchInput)
+
+    const url = `https://ai.github.com/search/users?q=${searchInput}`
+
+    try {
+      const response = await fetch(url)
+      if (!response.ok) {
+        throw Error(response.status)
+      }
+
+      const data = await response.json()
+      console.log(data.items[0])
+
+      setIsLoading(false)
+      if (data['total_count'] === 0) {
+        console.log('no data found')
+      } else {
+        setSearchResults(data.items)
+      }
+    } catch (err) {
+      setIsLoading(false)
+      handleErrors(err)
+    }
+  }
 
   return (
     <SafeAreaView>
-      <StatusBar />
-      <Text>Home</Text>
+      <View id='search'>
+        <StatusBar />
 
-      <TextInput
-        onChangeText={setSearchInput}
-        value={searchInput}
-        enterKeyHint={'search'}
-        multiline={'false'}
-        style={styles.input}
-      />
+        <Text>Home</Text>
+
+        <TextInput
+          onChangeText={setSearchInput}
+          value={searchInput}
+          placeholder={'Search users'}
+          enterKeyHint={'search'}
+          multiline={'false'}
+          style={styles.input}
+        />
+
+        <Button title='Search' onPress={handleSearch} />
+      </View>
     </SafeAreaView>
   )
 }
