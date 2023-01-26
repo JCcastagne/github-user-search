fade7e3b0c889cd093382c38c71559a932f5eaee
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2023-01-25 21:09:25 -0500

    Create User screen; render relevant information.

--- screens/User.js
+++ screens/User.js
@@ -1,11 +1,66 @@
-import { View, Text } from 'react-native'
+import { useEffect, useState } from 'react'
+import { View, Text, Image } from 'react-native'
 
 export default function User ({ route, navigation }) {
-  const params = route.params
-  console.log(params)
+  const { userId } = route.params
+  const [isLoading, setIsLoading] = useState(false)
+  const [userData, setUserData] = useState(null)
+
+  function handleErrors (error) {
+    console.log('User ERROR: ', error)
+  }
+
+  async function fetchUserData () {
+    setIsLoading(true)
+    console.log('userId: ', userId)
+
+    const url = `https://api.github.com/user/${userId}`
+
+    try {
+      const response = await fetch(url)
+      if (!response.ok) {
+        throw Error(response.status)
+      }
+
+      const data = await response.json()
+      console.log(data)
+
+      setIsLoading(false)
+      if (data.message === 'Not found' || !data.id) {
+        console.log('no data found')
+      } else {
+        setUserData(data)
+      }
+    } catch (err) {
+      setIsLoading(false)
+      handleErrors(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchUserData()
+  }, [userId])
+
+  if (!userData) {
+    return <Text>No user</Text>
+  }
+
   return (
     <View>
-      <Text>User</Text>
+      <View>
+        <Image
+          id='avatar'
+          source={{
+            uri: userData['avatar_url']
+          }}
+          style={{ width: 100, height: 100 }}
+        />
+        <Text>{`Name ${userData?.name}`}</Text>
+        <Text>{`Username ${userData?.login}`}</Text>
+      </View>
+      <Text>{`Bio ${userData?.bio || 'None'}`}</Text>
+      <Text>{`Following ${userData.following}`}</Text>
+      <Text>{`Followers ${userData.followers}`}</Text>
     </View>
   )
 }
