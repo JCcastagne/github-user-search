1fe54a348216d32c0bd0b9e59035c6eb63af7ee7
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2023-01-26 19:07:06 -0500

    Style following and followers buttons.

--- screens/User.js
+++ screens/User.js
@@ -66,36 +66,134 @@ export default function User ({ route, navigation }) {
           }}
           style={{ width: 100, height: 100 }}
         />
-        <Text>{`Name ${userData?.name}`}</Text>
+        <Text>{`Name ${userData?.name || 'N/A'}`}</Text>
         <Text>{`Username ${userData?.login}`}</Text>
       </View>
       <Text>{`Bio ${userData?.bio || 'None'}`}</Text>
 
-      <Pressable
-        id='following'
-        onPress={() => {
-          navigation.push('Users', {
-            login: userData?.login,
-            list: 'following'
-          })
-        }}
-      >
-        <Text>{`Following ${userData?.following}`}</Text>
-        <MaterialCommunityIcons name='chevron-right' size={24} color='black' />
-      </Pressable>
+      <View style={{ display: 'flex', flexDirection: 'row' }}>
+        <Pressable
+          id='following'
+          onPress={() => {
+            navigation.push('Users', {
+              login: userData?.login,
+              list: 'following'
+            })
+          }}
+          style={{
+            borderRadius: 12,
+            padding: 12,
+            marginRight: 16,
+            backgroundColor: styleVariables.colors.tertiaryContainer
+          }}
+        >
+          <MaterialCommunityIcons
+            name='account-group'
+            size={24}
+            color={styleVariables.colors.onTertiaryContainer}
+            style={{ marginBottom: 24 }}
+          />
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start'
+            }}
+          >
+            <Text
+              style={{
+                color: styleVariables.colors.onTertiaryContainer,
+                marginBottom: 4,
+                ...styleVariables.fontSizes.titleSmall
+              }}
+            >
+              {`Following ${userData?.following || 'none'}`}
+            </Text>
 
-      <Pressable
-        id='followers'
-        onPress={() => {
-          navigation.push('Users', {
-            login: userData?.login,
-            list: 'followers'
-          })
-        }}
-      >
-        <Text>{`Followers ${userData?.followers}`}</Text>
-        <MaterialCommunityIcons name='chevron-right' size={24} color='black' />
-      </Pressable>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row'
+              }}
+            >
+              <Text
+                style={{
+                  color: styleVariables.colors.onTertiaryContainer,
+                  ...styleVariables.fontSizes.bodySmall
+                }}
+              >
+                View
+              </Text>
+              <MaterialCommunityIcons
+                name='chevron-right'
+                size={16}
+                color={styleVariables.colors.onTertiaryContainer}
+              />
+            </View>
+          </View>
+        </Pressable>
+
+        <Pressable
+          id='followers'
+          onPress={() => {
+            navigation.push('Users', {
+              login: userData?.login,
+              list: 'followers'
+            })
+          }}
+          style={{
+            borderRadius: 12,
+            padding: 12,
+            marginRight: 16,
+            backgroundColor: styleVariables.colors.tertiaryContainer
+          }}
+        >
+          <MaterialCommunityIcons
+            name='tooltip-account'
+            size={24}
+            color={styleVariables.colors.onTertiaryContainer}
+            style={{ marginBottom: 24 }}
+          />
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start'
+            }}
+          >
+            <Text
+              style={{
+                color: styleVariables.colors.onTertiaryContainer,
+                marginBottom: 4,
+                ...styleVariables.fontSizes.titleSmall
+              }}
+            >
+              {`Followers ${userData?.followers || 'none'}`}
+            </Text>
+
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row'
+              }}
+            >
+              <Text
+                style={{
+                  color: styleVariables.colors.onTertiaryContainer,
+                  ...styleVariables.fontSizes.bodySmall
+                }}
+              >
+                View
+              </Text>
+              <MaterialCommunityIcons
+                name='chevron-right'
+                size={16}
+                color={styleVariables.colors.onTertiaryContainer}
+              />
+            </View>
+          </View>
+        </Pressable>
+      </View>
     </SafeAreaView>
   )
 }
